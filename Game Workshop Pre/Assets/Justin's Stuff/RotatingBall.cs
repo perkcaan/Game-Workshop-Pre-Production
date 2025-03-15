d0da3543af1d2ed3701fed2b25077334d47c0129
JustinSpace <hayjus0@gmail.com>
2025-03-15 17:25:16 -0400

    Rotating Ball

--- Game Workshop Pre/Assets/Justin's Stuff/RotatingBall.cs
+++ Game Workshop Pre/Assets/Justin's Stuff/RotatingBall.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RotatingBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 direction;
+    Vector3 up;
+    float speed = 100;
+
+    private void Start()
     {
-        
+        up = new Vector3(0, Mathf.Sqrt(2f) / 2f, -Mathf.Sqrt(2f) / 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Vector3 rotateAxis = Vector3.Cross(direction, up);
+
+        print(rotateAxis);
+
+        transform.Rotate(rotateAxis, -direction.magnitude * speed * Time.deltaTime, Space.World);
     }
 }
