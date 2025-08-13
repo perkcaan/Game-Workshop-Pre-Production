3e3ed3fa02b5043afd887311706481eed5ca0779
Igurbanov <87280887+igurbanov@users.noreply.github.com>
2025-08-12 23:16:20 -0500

    File Cleanup, Visual Scripting Removal

---
+++ Game Workshop Pre/Assets/Game/Trash/CollectableTrash.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CollectableTrash : MonoBehaviour
+{
+    public float trashSize;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.TryGetComponent(out TrashBallController trashBallController) && trashBallController.isAttached)
+        {
+            trashBallController.AddCollectableTrash(this);
+            DestroySelf();
+        }
+        // Ball collects trash independently
+        else if (collision.gameObject.TryGetComponent(out BallCollisionHandler ballHandler) && !ballHandler.trashBallController.isAttached)
+        {
+            // You can implement a method in BallCollisionHandler to handle trash collection,
+            // or forward it to the TrashBallController if needed.
+            ballHandler.trashBallController.AddCollectableTrash(this);
+            DestroySelf();
+
+
+
+        }
+
+        void DestroySelf()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
