01f4335406b5443f0091309978dc0314f2ceba66
DesolateNova <bclock@iu.edu>
2025-09-17 07:18:30 -0400

    ProgCom: Main Menu
    Creation of a functional but not feature complete main menu as well as start of a scalable loading screen system.

---
+++ Game Workshop Pre/Assets/Game/UI/MainMenuBehavior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+
+
+    [SerializeField] Button playButton;
+    [SerializeField] Button settingsButton;
+    [SerializeField] Button creditsButton;
+    [SerializeField] Button exitButton;
+
+
+
+    public void OnPlayButtonClicked()
+    {
+        Debug.Log("Play Clicked");
+        SceneManager.LoadScene("LoadingScreen");
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+        Debug.Log("Settings Clicked");
+    }
+
+    public void OnCreditsButtonClicked()
+    {
+        Debug.Log("Credits Clicked");
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Debug.Log("Exit Button Clicked");
+        if (Application.isPlaying)
+        {
+            Application.Quit();
+        }
+        else if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+    }
+
+
+}
