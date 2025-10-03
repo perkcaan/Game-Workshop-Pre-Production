a408fd51e2aeea418b46739d84ccf1ed6d3aba16
zachbaker55 <thezachbaker55@gmail.com>
2025-10-03 15:43:01 -0400

    Merged Caroline's heat shader into HeatMechanic & added flashing

--- Game Workshop Pre/Assets/Game/Mechanics/HeatMechanic.cs
+++ Game Workshop Pre/Assets/Game/Mechanics/HeatMechanic.cs
@@ -21,13 +21,31 @@ public class HeatMechanic : MonoBehaviour
 
     [Tooltip("The rate per second at which heat returns to room temperature.")]
     [SerializeField] private float _heatRelaxationRate = 0.5f;
+    [Tooltip("The heat level at which this object begins to glow.")]
+    [SerializeField] private int _warningThreshold = 60;
 
     [Tooltip("The heat level at which this object ignites into flame.")]
-    [SerializeField] private float _ignitionThreshold = 80;
+    [SerializeField] private int _ignitionThreshold = 80;
 
-    
 
+    [Tooltip("The maximum rate per second at which this object flashes.")]
+    [SerializeField] private float _maxFlashFrequency = 10f;
+
+    private float _flashPhase; // This is the time spent in the warning threshold. Used for shader.
+
+
+    // Components
     private Room _currentRoom;
+    private SpriteRenderer _spriteRenderer;
+    private MaterialPropertyBlock _block;
+
+
+    // Unity methods
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _block = new MaterialPropertyBlock();
+    }
 
     private void Start()
     {
@@ -37,6 +55,7 @@ public class HeatMechanic : MonoBehaviour
     private void Update()
     {
         RelaxHeat();
+        UpdateHeatShader();
     }
 
 
@@ -51,12 +70,18 @@ public class HeatMechanic : MonoBehaviour
     {
         if (_currentRoom == room)
         {
-            _currentRoom = null;   
+            _currentRoom = null;
         }
     }
 
 
     // Heat mechanics
+    //call to set heat
+    public void ModifyHeat(int change)
+    {
+        _heat = Mathf.Clamp(_heat + change, LOWEST_HEAT_VALUE, HIGHEST_HEAT_VALUE);
+    }
+
     private void SetHeatToRoom()
     {
         int roomTemperature = DistrictManager.Instance.Temperature;
@@ -75,10 +100,29 @@ public class HeatMechanic : MonoBehaviour
         }
     }
 
-
-    public void ModifyHeat(int change)
+    // update shader... Maybe move all the shader stuff into a shader controller if it becomes more than just heat related
+    private void UpdateHeatShader()
     {
-        _heat = Mathf.Clamp(_heat + change, LOWEST_HEAT_VALUE, HIGHEST_HEAT_VALUE);
+        // Get heat from _warningThreshold to _ignitionThreshold as 0-1 float 
+        float heat01 = Mathf.Clamp01((_heat - _warningThreshold) / (_ignitionThreshold - _warningThreshold));
+
+        // If above warning threshold, flash according to frequency and heat
+        if (_heat >= _warningThreshold)
+        {
+            float heatFlashRamp = 2f; // This can be serialized if we move
+            float xExp = Mathf.Pow(heat01, heatFlashRamp);
+            float flashSpeed = Mathf.Lerp(0, _maxFlashFrequency, xExp);
+            _flashPhase += Time.deltaTime * flashSpeed;
+        }
+        else
+        {
+            _flashPhase = 0f;
+        }
+
+        _spriteRenderer.GetPropertyBlock(_block);
+        _block.SetFloat("_Heat", heat01);
+        _block.SetFloat("_FlashPhase", _flashPhase);
+        _spriteRenderer.SetPropertyBlock(_block);
     }
 
 
