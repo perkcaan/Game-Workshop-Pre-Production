1911809e58dcec42586977c42db8aac576755a95
zachbaker55 <thezachbaker55@gmail.com>
2025-10-15 21:59:39 -0400

    Room polygon generator finished

--- Game Workshop Pre/Assets/Workspaces/Zach/DistrictManager.cs
+++ Game Workshop Pre/Assets/Workspaces/Zach/DistrictManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DistrictManager : StaticInstance<DistrictManager>
 {
     [SerializeField, Range(HeatMechanic.LOWEST_HEAT_VALUE, HeatMechanic.HIGHEST_HEAT_VALUE)] private int _baseTemperature;
-    public int Temperature { get { return _baseTemperature; } } 
+    public int Temperature { get { return _baseTemperature; } }
+
+    [SerializeField] private Tilemap _roomTilemap;
+
+    [ContextMenu("Generate Rooms")]
+    private void GenerateRooms()
+    {
+        RoomPolygonGenerator.GeneratePolygonColliders(transform, _roomTilemap);
+    }
 }
