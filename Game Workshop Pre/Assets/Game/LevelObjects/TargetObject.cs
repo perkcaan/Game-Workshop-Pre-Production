a79a7b10ca8aba7c8649be2bd84b8b64c94093b2
bnottage <126017316+bnottage@users.noreply.github.com>
2026-01-15 21:52:53 -0500

    commit1
    -Target Object
      -Destroyed when hit with an trash ball of appropriate size and speed
      -Calls an Event when destroyed
    
    -Dumpster Object
      -When the player is near, pressing 'E' will cause the Dumpster to shake
      -The Dumpster stores an Item. There is a function included that can be modified to drop the Item or add it directly to the Inventory

---
+++ Game Workshop Pre/Assets/Game/LevelObjects/TargetObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetObject : MonoBehaviour
+{
+
+    // Velocity needed to break the object
+    [SerializeField] private float requiredVelocity;
+
+    // Size needed to break the object
+    [SerializeField] private float requiredSize;
+
+    // Target Object's event
+    [SerializeField] private UnityEvent BreakEvent;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check that object is Trash Ball
+        // and that its Size and Speed are equal to or exceed the Target Object's
+        if (collision.gameObject.GetComponent<TrashBall>() == true)
+        {
+            if (collision.gameObject.GetComponent<TrashBall>().Size >= requiredSize 
+                && Vector3.Magnitude(collision.gameObject.GetComponent<Rigidbody2D>().velocity) >= requiredVelocity)
+            {
+                BreakEvent?.Invoke(); // Call associated event
+            }   
+        }
+    }
+
+
+}
